9c283d2030fa8a8ca8343055dea3f0ad71c0a70e
Asker Kakhramanov <62985982+Gakhramanzode@users.noreply.github.com>
2022-12-27 11:44:52 +0300

    Issues 8 (#9)
    * issues-8: добавил флаг '-e' цвета в файлах src.sh, src-centos.sh
    
    * issues-8: исправил дефект в файлах src.sh, src-centos.sh, src-ubuntu.sh
    
    * issues-8: протестировал все изменения

--- src.sh
+++ src.sh
@@ -3,27 +3,23 @@
 set -e
 
 #Переменный цвета
+GREEN='\033[0;32m' #Зеленый
 RED='\033[0;31m' #Красный
 NC='\033[0m' #Нет цвета
 
 ID=`cat /etc/os-release | grep ^ID=`
 
 if [[ "$ID" = "ID=ubuntu" ]]; then
-	clear
-	echo "У вас Ubuntu"
+	echo -e "${GREEN}У вас Ubuntu${NC}"
 	sleep 3
-	clear
         chmod +x src-ubuntu.sh
         ./src-ubuntu.sh
 elif [[ "$ID" = 'ID="centos"' ]]; then
-	clear
-	echo "У вас CentOS"
+	echo -e "${GREEN}У вас CentOS${NC}"
         sleep 3
-        clear
         chmod +x src-centos.sh
         ./src-centos.sh
 else
-
-        echo "${RED}Версия операционной системы не поддерживается.${NC}"
+        echo -e "${RED}Версия операционной системы не поддерживается.${NC}"
         exit 1
 fi
